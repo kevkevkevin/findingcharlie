842b5e1dcc5d7ee94b7dc5187139628a6c132608
kevin <kevinbulfanisay1@gmail.com>
2024-11-14 21:57:04 +0300

    Add files via upload

---
+++ js/dogs.js
@@ -0,0 +1,22 @@
+
+var pug_arr = ['assets/dogs/pug1.png', 'assets/dogs/pug2.png'];
+var dogs_arr = ['assets/dogs/type1a.png', 'assets/dogs/type2a.png', 'assets/dogs/type4a.png', 'assets/dogs/type3a.png' ];
+				
+var levels_arr = [{frameWidth:600, frameHeight:600, scale:1, timer:15000},
+				{frameWidth:600, frameHeight:600, scale:.95, timer:14000},
+				{frameWidth:600, frameHeight:600, scale:.9, timer:13000},
+				{frameWidth:600, frameHeight:600, scale:.85, timer:12000},
+				{frameWidth:600, frameHeight:600, scale:.8, timer:11000},
+				{frameWidth:700, frameHeight:600, scale:.8, timer:10000},
+				{frameWidth:800, frameHeight:600, scale:.8, timer:9000},
+				{frameWidth:900, frameHeight:600, scale:.8, timer:8000},
+				{frameWidth:900, frameHeight:600, scale:.8, timer:7000},
+				{frameWidth:900, frameHeight:600, scale:.8, timer:6000},
+				{frameWidth:900, frameHeight:600, scale:.8, timer:5000},
+				{frameWidth:900, frameHeight:600, scale:.8, timer:4000},
+				{frameWidth:950, frameHeight:600, scale:.7, timer:4000},
+				{frameWidth:950, frameHeight:600, scale:.7, timer:4000},
+				{frameWidth:950, frameHeight:600, scale:.6, timer:3000},
+				{frameWidth:950, frameHeight:600, scale:.6, timer:3000},
+				{frameWidth:1000, frameHeight:600, scale:.5, timer:1000},
+				{frameWidth:1000, frameHeight:600, scale:.5, timer:1000},];
